97f9dcc2e306fb5e44f0a79cdc4cb05014270191
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2025-03-14 13:53:21 +0300

    Merge branch 'add_load_screen'

---
+++ lib/feature/post_grid/ui/post_loader.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:postogram/feature/post_grid/ui/loading_screen.dart';
+import 'package:postogram/feature/post_grid/ui/post_grid_widget_model.dart';
+import 'package:postogram/feature/post_grid/ui/widget/post_card.dart';
+
+class PostsLoader extends StatelessWidget {
+  const PostsLoader({super.key, required this.wm});
+
+  final IPostGridWidgetModel wm;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: wm.posts,
+      builder: (_, posts, __) => posts.isEmpty
+          ? const LoadingScreen()
+          : GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: posts.length,
+              itemBuilder: (_, index) => PostCard(
+                post: posts[index],
+                onPostOpen: () => wm.openDetails(posts[index]),
+              ),
+            ),
+    );
+  }
+}
